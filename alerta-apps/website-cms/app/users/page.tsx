eb9612813b11a8bbb8d8dfa4401a703e7cd58cee
codebyyanto <yanto.unix@gmail.com>
2026-05-29 15:10:44 +0700

    Menambahkan empty state pengguna

--- alerta-apps/website-cms/app/users/page.tsx
+++ alerta-apps/website-cms/app/users/page.tsx
@@ -180,3 +180,4 @@ export default function UserManagementPage() {
             {
               loading ? (
                 <div className="flex flex-col items-center justify-center py-20"></div>
+              ) : filteredUsers.length === 0 ? (
