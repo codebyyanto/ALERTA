c8befb08b00f1c8f75994df7a01caeb06c42095b
codebyyanto <yanto.unix@gmail.com>
2026-05-29 14:00:01 +0700

    memperbaiki fitur show dan hide pada input password web cms

--- alerta-apps/website-cms/app/login/page.tsx
+++ alerta-apps/website-cms/app/login/page.tsx
@@ -87,7 +87,7 @@ export default function LoginPage() {
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-4 top-1/2 -translate-y-1/2 text-slate-300 hover:text-slate-500 transition-colors"
+                className="absolute right-4 top-1/2 -translate-y-1/2 text-slate-300 hover:text-slate-500 transition-colors z-10"
               >
                 {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
               </button>
