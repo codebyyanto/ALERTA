36726f07a3d9d9d23e5c9a0da4b315126b2993ef
fatimahlailatulazzahra <afatim915@gmail.com>
2026-05-30 16:12:53 +0700

    import komponen utama dari react-native

--- alerta-apps/mobile/app/articles/[id].tsx
+++ alerta-apps/mobile/app/articles/[id].tsx
@@ -1,1 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  ActivityIndicator,
+  Dimensions,
+  Platform,
+  StatusBar,
+} from 'react-native';
