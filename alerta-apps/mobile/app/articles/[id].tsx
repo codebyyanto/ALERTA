01675fe92caa5340308b0e2ed54d440d3870b569
fatimahlailatulazzahra <afatim915@gmail.com>
2026-05-30 16:14:11 +0700

    import router dan icon dari lucide-react

--- alerta-apps/mobile/app/articles/[id].tsx
+++ alerta-apps/mobile/app/articles/[id].tsx
@@ -12,3 +12,5 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'expo-image';
+import { useLocalSearchParams, router } from 'expo-router';
+import { ArrowLeft, Calendar, Eye, ShieldCheck, Share2, Bookmark } from 'lucide-react-native';
