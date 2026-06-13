5d57803ad2ed90e32fd4cfad81b3ee5c419e88c0
codebyyanto <yanto.unix@gmail.com>
2026-06-13 21:53:47 +0700

    memperbarui middleware nextjs untuk memproteksi halaman manajemen laporan

--- website-cms/middleware.ts
+++ website-cms/middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/login', '/dashboard', '/monitoring'],
+  matcher: ['/', '/login', '/dashboard', '/monitoring', '/reports'],
 };
