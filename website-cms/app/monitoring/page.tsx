0091fa2e0a864a5398d2275f52ea0b78249a970a
codebyyanto <yanto.unix@gmail.com>
2026-06-13 21:32:50 +0700

    membuat rute dan komponen halaman pemantauan bencana baru

---
+++ website-cms/app/monitoring/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function MonitoringPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Disaster Monitoring</h1>
+    </div>
+  );
+}
